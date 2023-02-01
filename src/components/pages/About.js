7241dd0a251a27438af2a754db50a60fd81db19d
Saloni <Saloniwagh2@gmail.com>
2023-02-01 13:27:47 +0530

    commit1

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ function About() {
     <Segment>
       <Header as="h4" content="Github Finder React App" />
       <p>Version: 1.0.0</p>
-      <p>Developed By: Mahima
+      <p>Developed By: Saloni Wagh.
       </p>
       <p>Inspired From: Griffindor community</p>
     </Segment>
